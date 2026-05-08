4c7c511739f0ad4da2d289f33cdfedfcf1c409ca
Mohamed Tawil <twlmed212@gmail.com>
2026-05-08 21:40:33 +0100

    cleanignProject

--- module-03/ex01/main.cpp
+++ module-03/ex01/main.cpp
@@ -1,7 +1,7 @@
 #include "ScavTrap.hpp"
 
 int main() {
-    ScavTrap s("Said");   // should show ClapTrap created THEN ScavTrap created
+    ScavTrap s("Said");
     s.attack("target");
     s.guardGate();
-}                         // should show ScavTrap destroyed THEN ClapTrap destroyed
+}
